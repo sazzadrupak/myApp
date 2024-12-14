1992536761d11beb4f33f45e2851e6d71250c789
sazzadrupak <sazzad@oployeelabs.com>
2024-12-14 21:30:47 +0200

    relative and absolute positioning

--- App.js
+++ App.js
@@ -27,31 +27,23 @@ export default function App() {
         <View
           style={{
             backgroundColor: 'dodgerblue',
-            // flex: 1,
-            // flex: 2,
-            // width: 100,
-            // flexBasis: 100,
-            // flexGrow: 1, // same as flex: 1
-            width: 300,
-            flexShrink: 1,
+            width: 100,
             height: 100,
-            // height: 300, // for baseline align items
-            // alignSelf: 'flex-start',
           }}
         />
         <View
           style={{
             backgroundColor: 'gold',
-            flex: 1,
             width: 100,
             height: 100,
-            // height: 200, // for baseline align items
+            top: 20,
+            left: 20,
+            position: 'absolute',
           }}
         />
         <View
           style={{
             backgroundColor: 'tomato',
-            flex: 1,
             width: 100,
             height: 100,
           }}
