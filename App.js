b0e272d317f08f30c6d12a32da168272c74186b1
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 21:01:32 +0200

    Stack Navigator

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Text } from 'react-native';
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+
+const TweetsScreen = () => (
+  <Screen>
+    <Text>TWEET</Text>
+  </Screen>
+);
+
+const TweetDetailsScreen = () => (
+  <Screen>
+    <Text>TWEET DETAILS</Text>
+  </Screen>
+);
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Tweets',
+  screenOptions: {
+    headerStyle: { backgroundColor: 'tomato' },
+  },
+  screens: {
+    Tweets: {
+      screen: TweetsScreen,
+      options: {
+        title: 'Overview',
+      },
+    },
+    TweetDetails: TweetDetailsScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <Navigation />;
 }
