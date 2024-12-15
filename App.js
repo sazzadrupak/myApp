6e3ba7077b52ab0f03b4dcb3886e209e759483d9
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 19:22:40 +0200

    Building ImageInput Touches

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as ImagePicker from 'expo-image-picker';
 import React, { useEffect, useState } from 'react';
 
-import { Button, Image } from 'react-native';
 import ImageInput from './app/components/ImageInput';
 import Screen from './app/components/Screen';
 
@@ -28,9 +27,10 @@ export default function App() {
 
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      <ImageInput imageUri={imageUri} />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,17 +1,59 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useEffect } from 'react';
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
 import colors from '../config/colors';
 
-const ImageInput = ({ imageUri }) => {
+const ImageInput = ({ imageUri, onChangeImage }) => {
+  const requesPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library.');
+  };
+
+  useEffect(() => {
+    requesPermission();
+  }, []);
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert('Delete', 'Are you sure you want to delete this image?', [
+        { text: 'Yes', onPress: () => onChangeImage(null) },
+        { text: 'No' },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        quality: 0.5,
+      });
+      if (!result.canceled) onChangeImage(result.assets[0].uri);
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons name="camera" size={40} color={colors.medium} />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
