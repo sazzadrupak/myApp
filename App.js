e38ad3901122071489ba2d63d6efbf3257d42eef
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 19:05:34 +0200

    Accessing expo image library

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useEffect, useState } from 'react';
+
+import { Button, Image } from 'react-native';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+  const requesPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library.');
+  };
+
+  useEffect(() => {
+    requesPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      console.log(result);
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
