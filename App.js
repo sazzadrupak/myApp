206c5394f4e6ece0d0349962dca63910ffe6d57f
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 14:11:13 +0200

    Building TextInput

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ListingsScreen from './app/screens/ListingsScreen';
+import React, { useState } from 'react';
+import AppTextInput from './app/components/AppTextInput';
+import Screen from './app/components/Screen';
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState('');
+  return (
+    <Screen>
+      <AppTextInput placeholder="First Name" icon="email" />
+    </Screen>
+  );
 }
