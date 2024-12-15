1efa5849df3429f56a154804ee829bbb1023f5a8
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 14:27:43 +0200

    Custom picker

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import AppPicker from './app/components/AppPicker';
 import AppTextInput from './app/components/AppTextInput';
 import Screen from './app/components/Screen';
+
 export default function App() {
   const [firstName, setFirstName] = useState('');
   return (
     <Screen>
-      <AppTextInput placeholder="First Name" icon="email" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="First Name" />
     </Screen>
   );
 }
