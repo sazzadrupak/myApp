dddcd3a26349808239594393040b771f8342a8ab
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 07:14:57 +0200

    Building Listing details screen

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-import { View } from 'react-native';
-
-import Card from './app/components/Card';
+import ListingDetailScreen from './app/screens/ListingDetailScreen';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#f8f4f4',
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subtitle="$100"
-        image={require('./app/assets/jacket.jpg')}
-      />
-    </View>
-  );
+  return <ListingDetailScreen />;
 }
