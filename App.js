5e63712e6ea56af5732370a7d9ab73f021facfd8
sazzadrupak <sazzad@oployeelabs.com>
2024-12-15 10:16:33 +0200

    FLatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
